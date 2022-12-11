98bc58137c95038c39f5b88f7b6a66e7cd2a9235
ljlima <limajeanlucas@gmail.com>
2022-12-10 21:57:43 -0400

    curso javascript front end

---
+++ midi/main.js
@@ -0,0 +1,22 @@
+function tocaSom (idTeclaAudio) {
+    document.querySelector(idTeclaAudio).play();
+}
+
+const ListaDeTeclas = document.querySelectorAll('.tecla');    
+
+
+
+let contador = 0;
+
+for (let contador=0 ; contador < ListaDeTeclas.length ; contador++){
+    const tecla = ListaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //Template String
+    const audio = `#som_${instrumento}`;
+
+    //função anonima
+    tecla.onclick = function (){
+        tocaSom(audio);
+    };
+
+}
